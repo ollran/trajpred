eab5471a46808966e1e0d89af02fcbffcf11e722
Olli Rantinoja <ollran@iki.fi>
2020-06-14 14:30:13 +0300

    Rename parameters and variables

--- preprocessor/main.py
+++ preprocessor/main.py
@@ -17,31 +17,31 @@ HASH_SHA2 = 'HASH_SHA2'
 DIRECTORY = 'DIRECTORY'
 
 
-def fetch_dataset(url: str, archive_filename: str) -> None:
+def fetch_dataset(url: str, dataset_archive_filename: str) -> None:
     """
     Fetches the dataset from the URL and saves it into a file
     :param url: the URL of the dataset
-    :param archive_filename: the destination filename
+    :param dataset_archive_filename: the destination filename
     :return: None
     """
     with get(url=url, stream=True) as response, \
-            open(archive_filename, mode='wb') as file_handle:
+            open(dataset_archive_filename, mode='wb') as file_handle:
         for chunk in response.iter_content(chunk_size=128):
             file_handle.write(chunk)
 
 
-def verify_dataset(dataset_archive_filename: str, dataset_hash_sha2: str) -> bool:
+def verify_dataset(dataset_archive_filename: str, dataset_archive_hash_sha2: str) -> bool:
     """
     Verifies the SHA2 hash of the dataset archive
     :param dataset_archive_filename: the filename of the ZIP archive
-    :param dataset_hash_sha2: the SHA2 hash of the dataset ZIP archive
+    :param dataset_archive_hash_sha2: the SHA2 hash of the dataset ZIP archive
     :return: True of False depending on the result of the check
     """
     hash_sha2 = sha256()
     with open(dataset_archive_filename, mode='rb') as file_handle:
         for chunk in iter(lambda: file_handle.read(4096), b''):
             hash_sha2.update(chunk)
-        return hash_sha2.hexdigest() == dataset_hash_sha2
+        return hash_sha2.hexdigest() == dataset_archive_hash_sha2
 
 
 def extract_dataset(dataset_archive_filename: str, dataset_directory: str) -> None:
@@ -77,9 +77,9 @@ def main() -> None:
     config = ConfigParser()
     config.read('config.ini')
     dataset_url = config[DATASET][URL]
-    dataset_hash_sha2 = config[DATASET][HASH_SHA2]
-    dataset_directory = config[DATASET][DIRECTORY]
     dataset_archive_filename = dataset_url.split('/')[-1]
+    dataset_archive_hash_sha2 = config[DATASET][HASH_SHA2]
+    dataset_directory = config[DATASET][DIRECTORY]
 
 
 if __name__ == '__main__':
