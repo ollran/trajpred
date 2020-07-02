c6dea74db6534460a30acdc63aa5d5d3b0c43bf1
Olli Rantinoja <ollran@iki.fi>
2020-07-02 20:49:30 +0300

    Add speed related utility functions

---
+++ notebooks/utils/speed.py
@@ -0,0 +1,26 @@
+# pylint: disable=E0402
+
+"""
+Speed related functions
+"""
+
+from numpy import ndarray, size
+from .distance import calculate_distance_in_meters
+
+
+def calculate_speed_in_ms(start: ndarray, end: ndarray) -> float:
+    """
+    Calculate speed between 2 adjacent coordinates
+    :param start: start coordinate
+    :param end: end coordinate
+    :return: speed m/s
+    """
+    assert size(start, 0) == 4
+    assert size(end, 0) == 4
+
+    [_, _, ts_start, _] = start
+    [_, _, ts_end, _] = end
+    return calculate_distance_in_meters(
+        start=start,
+        end=end
+    ) / ((ts_end - ts_start) / 1000)  # ms -> s
