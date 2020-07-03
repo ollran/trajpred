567f812ce65e72a6d2a4d55cf76892802e356b20
Olli Rantinoja <ollran@iki.fi>
2020-07-03 13:08:43 +0300

    Implement user's trajectory loading function

--- notebooks/utils/dataset.py
+++ notebooks/utils/dataset.py
@@ -2,6 +2,9 @@
 Dataset related functions
 """
 
+from itertools import chain
+from os import fwalk
+from typing import List
 from numpy import loadtxt, ndarray
 
 # CONSTANTS
@@ -21,3 +24,17 @@ def load_trajectory(user_id: int, trajectory_id: int) -> ndarray:
     return loadtxt(
         f'{DATASET_DIRECTORY}/{user_id}/{trajectory_id}'
     )
+
+
+def load_users_trajectories(user_id: int) -> List[ndarray]:
+    """
+    Loads all user's trajectories and returns them as a list of numpy arrays
+    :param user_id: user's id
+    :return: List of trajectories
+    """
+    assert user_id > 0
+
+    return list(chain(*[
+        [loadtxt(f'{dirpath}/{f}') for f in filenames]
+        for dirpath, _, filenames, _ in fwalk(f'{DATASET_DIRECTORY}/{user_id}')
+    ]))
