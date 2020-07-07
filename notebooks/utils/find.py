1b8f0ed9687e2f186ddf37e8fb528431b0a7dae5
Olli Rantinoja <ollran@iki.fi>
2020-07-07 14:15:39 +0300

    Fix assertions in find_target_point_indices() function

--- notebooks/utils/find.py
+++ notebooks/utils/find.py
@@ -98,8 +98,8 @@ def find_target_point_indices(trajectory: ndarray, target_point: ndarray) -> nda
     :param target_point: target point
     :return: indices as ndarray
     """
-    assert size(trajectory, 0) == 4
-    assert size(trajectory, 1) > 0
+    assert size(trajectory, 0) > 0
+    assert size(trajectory, 1) == 4
     assert size(target_point, 0) == 4
 
     return where(all(
