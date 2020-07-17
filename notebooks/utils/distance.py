ca8ad4941e20b51e5f661d7f2e8d30803a857ceb
Olli Rantinoja <ollran@iki.fi>
2020-07-17 10:06:48 +0300

    Implement calculate_trajectory_length_in_meters() function

--- notebooks/utils/distance.py
+++ notebooks/utils/distance.py
@@ -2,7 +2,9 @@
 Trajectory distance related functions
 """
 
+from functools import reduce
 from math import radians
+from typing import Tuple
 from numpy import arcsin, cos, ndarray, sqrt, sin, size
 
 
@@ -25,3 +27,29 @@ def calculate_distance_in_meters(start: ndarray, end: ndarray) -> float:
     return 2 * radius * arcsin(sqrt(
         sin(lat * 0.5) ** 2 + cos(lat_start) * cos(lat_end) * sin(lon * 0.5) ** 2
     )) * 1000  # meters
+
+
+def calculate_trajectory_length_in_meters(trajectory: ndarray) -> float:
+    """
+    Calculates the length of the trajectory in meters
+    :param trajectory: trajectory
+    :return: length in meters
+    """
+    assert size(trajectory, 0) >= 2
+    assert size(trajectory, 1) == 4
+
+    def reducer(acc: Tuple[float, ndarray], cur: ndarray) -> Tuple[float, ndarray]:
+        distance, last_coordinate = acc
+        current_distance = calculate_distance_in_meters(
+            start=last_coordinate,
+            end=cur
+        )
+        return distance + current_distance, cur
+
+    total_distance, _ = reduce(
+        reducer,
+        trajectory[1:],
+        (0.0, trajectory[0])
+    )
+
+    return total_distance
