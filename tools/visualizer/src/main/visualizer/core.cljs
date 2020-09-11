8fd6099649d33ac245b41dbd4fd68c38d8aef8b4
Olli Rantinoja <ollran@iki.fi>
2020-09-11 11:33:19 +0300

    Render leaflet map when initializing visualizing tool

--- tools/visualizer/src/main/visualizer/core.cljs
+++ tools/visualizer/src/main/visualizer/core.cljs
@@ -1,5 +1,13 @@
 (ns visualizer.core)
 
-(defn init []
-  (println "Hello world"))
+(def L js/L)
 
+(def tilelayer-settings
+  (js-obj "maxZoom" 19
+          "attribution" "&copy; <a href=\"https://openstreetmap.org/copyright\">OpenStreetMap contributors</a>"))
+
+(defn init []
+  (let [l-map (.map L "mapid")
+        l-tilelayer (.tileLayer L "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" tilelayer-settings)]
+    (.setView l-map (array 62.601090 29.763530) 13)
+    (.addTo l-tilelayer l-map)))
