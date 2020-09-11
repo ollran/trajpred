9ada2da990608432c813b2b19390bbe4b4680a8a
Olli Rantinoja <ollran@iki.fi>
2020-09-11 13:30:49 +0300

    Implement coordinate parsing and rendering

--- tools/visualizer/src/main/visualizer/core.cljs
+++ tools/visualizer/src/main/visualizer/core.cljs
@@ -1,15 +1,38 @@
-(ns visualizer.core)
+(ns visualizer.core
+  (:require [clojure.string :as str]))
 
 (def L js/L)
+
+(defonce L-map (.map L "mapid"))
 
 (def tilelayer-settings
   (js-obj "maxZoom" 19
           "attribution" "&copy; <a href=\"https://openstreetmap.org/copyright\">OpenStreetMap contributors</a>"))
 
 (defn init []
-  (let [l-map (.map L "mapid")
-        l-tilelayer (.tileLayer L "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" tilelayer-settings)
+  (let [l-tilelayer (.tileLayer L "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" tilelayer-settings)
         l-scale (.control.scale L)]
-    (.setView l-map (array 62.601090 29.763530) 13)
-    (.addTo l-tilelayer l-map)
-    (.addTo l-scale l-map)))
+    (.setView L-map (array 62.601090 29.763530) 13)
+    (.addTo l-tilelayer L-map)
+    (.addTo l-scale L-map)))
+
+(defn parse-raw-input [raw-input]
+  (as-> raw-input i
+    (str/split i "\n")
+    (map #(str/split % ",") i)
+    (filter #(> (count %) 1) i)
+    (map #(apply array %) i)
+    (apply array i)))
+
+(defn draw-path [path color]
+  (let [polyline (.polyline L path (js-obj "color" color))
+        bounds (.getBounds polyline)]
+    (.addTo polyline L-map)
+    (.fitBounds L-map bounds)))
+
+(defn add [id]
+  (let [raw-input (-> js/document
+                      (.getElementById id)
+                      (.-value))
+        parsed-input (parse-raw-input raw-input)]
+    (draw-path parsed-input "red")))
