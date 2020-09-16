1e096f1162450ec7c18b7888b8ba0f2655fa4ff0
Olli Rantinoja <ollran@iki.fi>
2020-09-16 10:35:33 +0300

    Improve visualization tool, parse all trajectories as one

--- tools/visualizer/src/main/visualizer/core.cljs
+++ tools/visualizer/src/main/visualizer/core.cljs
@@ -1,30 +1,30 @@
 (ns visualizer.core
   (:require [clojure.string :as str]))
 
-(def L js/L)
+(def L
+  js/L)
 
-(defonce L-map (.map L "mapid"))
+(defonce L-map
+  (.map L "mapid"))
+
+(defonce polygon-layer-group
+  (.layerGroup L))
 
 (def tilelayer-settings
   (js-obj "maxZoom" 19
           "attribution" "&copy; <a href=\"https://openstreetmap.org/copyright\">OpenStreetMap contributors</a>"))
 
-(def polygon-layer-group (.layerGroup L))
+(def colors
+  ["black" "blue" "red"])
 
-(defn init []
-  (let [l-tilelayer (.tileLayer L "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" tilelayer-settings)
-        l-scale (.control.scale L)]
-    (.setView L-map (array 62.601090 29.763530) 13)
-    (.addTo l-tilelayer L-map)
-    (.addTo l-scale L-map)))
-
-(defn parse-raw-input [raw-input]
-  (as-> raw-input i
-    (str/split i "\n")
-    (map #(str/split % ",") i)
-    (filter #(> (count %) 1) i)
-    (map #(apply array %) i)
-    (apply array i)))
+(defn parse-input [input]
+  (as-> input i
+    (str/split i "---")
+    (map #(str/split % "\n") i)
+    (map (fn [n] (map (fn [m] (str/split m ",")) n)) i)
+    (map (fn [n] (filter (fn [m] (> (count m) 1))  n)) i)
+    (map (fn [n] (map (fn [m] (apply array m)) n)) i)
+    (map #(apply array %) i)))
 
 (defn draw-path [path color]
   (let [polyline (.polyline L path (js-obj "color" color))
@@ -33,17 +33,23 @@
     (.addTo polygon-layer-group L-map)
     (.fitBounds L-map bounds)))
 
-(defn ^:export add [id]
-  (let [raw-input (-> js/document
-                      (.getElementById id)
-                      (.-value))
-        parsed-input (parse-raw-input raw-input)
-        color (cond (= id "head") "black"
-                    (= id "tail") "blue"
-                    (= id "pred") "red")]
-    (when (not-empty parsed-input)
-      (draw-path parsed-input color))))
+(defn init []
+  (let [l-tilelayer (.tileLayer L "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" tilelayer-settings)
+        l-scale (.control.scale L)]
+    (.setView L-map (array 62.601090 29.763530) 13)
+    (.addTo l-tilelayer L-map)
+    (.addTo l-scale L-map)))
 
-(defn ^:export clear-all []
+(defn ^:export draw []
+  (let [input (-> js/document
+                  (.getElementById "input")
+                  (.-value))
+        parsed-input (parse-input input)]
+    (doseq [[path color]
+            (map list parsed-input colors)]
+      (when (not-empty path)
+        (draw-path path color)))))
+
+(defn ^:export clear []
   (.remove polygon-layer-group)
   (.clearLayers polygon-layer-group))
