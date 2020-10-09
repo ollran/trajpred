49769a2fe252ead65f0c4b32b56b1508cd5962aa
Olli Rantinoja <ollran@iki.fi>
2020-10-09 09:47:11 +0300

    Add initial router

---
+++ tools/servant/lib/servant/router.ex
@@ -0,0 +1,24 @@
+defmodule Servant.Router do
+  # https://hexdocs.pm/plug/Plug.Router.html
+
+  use Plug.Router
+  require Logger
+
+  plug(:match)
+  plug(:dispatch)
+
+  get("/", do: send_resp(conn, 200, "found"))
+  match(_, do: send_resp(conn, 404, "not found"))
+
+  def child_spec(opts),
+    do: %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]},
+      type: :worker,
+      restart: :permanent,
+      shutdown: 500
+    }
+
+  def start_link(_opts),
+    do: Plug.Adapters.Cowboy2.http(__MODULE__, [])
+end
