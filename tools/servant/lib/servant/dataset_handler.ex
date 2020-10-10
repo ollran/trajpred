0c08b5ad23d0100007865015b9f0eaacfd9cdf10
Olli Rantinoja <ollran@iki.fi>
2020-10-10 08:51:23 +0300

    No need to print anything

--- tools/servant/lib/servant/dataset_handler.ex
+++ tools/servant/lib/servant/dataset_handler.ex
@@ -1,8 +1,5 @@
 defmodule Servant.DatasetHandler do
   def fetch_trajectory(user_id, trajectory_id) do
-    IO.puts(user_id)
-    IO.puts(trajectory_id)
-
     Path.join(["../../dataset", user_id, trajectory_id])
     |> Path.expand()
     |> File.read()
