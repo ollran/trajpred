3ecd9599ff62fba41db2a5af7cf5771f32f3edcb
Olli Rantinoja <ollran@iki.fi>
2020-11-13 16:05:50 +0200

    Predictor: add main.py file

---
+++ tools/predictor/main.py
@@ -0,0 +1,15 @@
+"""
+CLI utility for predictions and reports
+"""
+
+
+def main() -> None:
+    """
+    Main procedure of the predictor
+    :return: None
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
