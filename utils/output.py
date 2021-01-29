29b3a63ad395b43f48250b92a608856d29fbdc95
Olli Rantinoja <ollran@iki.fi>
2021-01-29 16:18:33 +0200

    Implement result filename function

--- utils/output.py
+++ utils/output.py
@@ -5,6 +5,8 @@ Outputting related functions
 from typing import List
 from numpy import ndarray, size
 
+RESULT_DIRECTORY = './results'
+
 
 def output_coordinates_to_file(filename: str, results: ndarray) -> None:
     """
@@ -39,3 +41,29 @@ def output_all_coordinates_to_file(filename: str, *results: List[ndarray]) -> No
                 [lat, lon, _, _] = line
                 file.write(f'{lat},{lon}\n')
             file.write('---\n')
+
+
+def get_result_filename(
+        user_id: int,
+        trajectory_id: int,
+        ratio: int,
+        method: str,
+        prediction_time: int,
+) -> str:
+    """
+    Create a result filename as a string
+    :param user_id: User id
+    :param trajectory_id: Trajectory id
+    :param ratio: Split ratio
+    :param method: Prediction method
+    :param prediction_time: Prediction time
+    :return: Result filename string
+    """
+    assert user_id > 0
+    assert trajectory_id > 0
+    assert ratio >= 0.0
+    assert ratio <= 1.0
+    assert method
+    assert prediction_time > 0
+
+    return f'{RESULT_DIRECTORY}/{user_id}_{trajectory_id}_{ratio}_{method}_{prediction_time}.txt'
